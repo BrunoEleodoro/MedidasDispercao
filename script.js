1481fc7db98e294fe03c04ecf7c65b600bc7810e
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2017-11-24 21:49:24 -0200

    Add files via upload
    Implementação dos calculos das tabelas com ROL, tabela de dados agrupados e tabela de classe

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+
+=======
+>>>>>>> 5668fd02416346ff54fb30cf22d93c13a204c746
 function gerarCampo(campoPai)
 {
   if (document.getElementById('div-detalhes')) {
@@ -8,8 +12,13 @@ function gerarCampo(campoPai)
   {
     var newdiv = document.createElement('div');
     newdiv.setAttribute("id", "div-detalhes");
+<<<<<<< HEAD
+    newdiv.innerHTML = "<br>Insira o número de elementos <br><input type='number' name='linhas' id='linhas' min='3' max='100'>"+
+                        "<br><input type='button' value='Gerar Espaços' onclick='proxima_pagina();'>";
+=======
     newdiv.innerHTML = "<br>Insira o número de elementos <br><input type='number' name='numCampos' min='3' max='100'>"+
                         "<br><input type='button' value='Gerar Espaços'>";
+>>>>>>> 5668fd02416346ff54fb30cf22d93c13a204c746
     document.getElementById("insercaoDados").appendChild(newdiv);
   }
 
@@ -17,8 +26,13 @@ function gerarCampo(campoPai)
   {
     var newdiv = document.createElement('div');
     newdiv.setAttribute("id", "div-detalhes");
+<<<<<<< HEAD
+    newdiv.innerHTML = "<br>Insira o número de linhas<br><input type='number' name='linhas' id='linhas' min='3' max='30'>"+
+                        "<br><input type='button' value='Gerar Linhas' onclick='proxima_pagina();'>";;
+=======
     newdiv.innerHTML = "<br>Insira o número de linhas<br><input type='number' name='numLinhas' min='3' max='30'>"+
                         "<br><input type='button' value='Gerar Linhas'>";;
+>>>>>>> 5668fd02416346ff54fb30cf22d93c13a204c746
     document.getElementById("insercaoDados").appendChild(newdiv);
   }
 }
@@ -29,3 +43,24 @@ function limparDetalhes()
   var child = document.getElementById("div-detalhes");
   parent.removeChild(child);
 }
+<<<<<<< HEAD
+function proxima_pagina()
+{
+  var dropdown1 = document.getElementById("tipoInsercao");
+  var tipoSelecionado = dropdown1.options[dropdown1.selectedIndex].value;
+  var elementos = document.getElementById("linhas").value;
+  if(tipoSelecionado == 1)
+  {
+      window.location="gerar_rol.php?linhas="+elementos;
+  }
+  else if(tipoSelecionado == 2)
+  {
+      window.location="gerar.php?linhas="+elementos;
+  }
+  else
+  {
+      window.location="gerar_classe.php?linhas="+elementos;   
+  }
+}
+=======
+>>>>>>> 5668fd02416346ff54fb30cf22d93c13a204c746
